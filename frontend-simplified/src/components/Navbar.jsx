4770180eb4e7fc4375637f3e0b4ac47ccaf8300e
RoshiniF <roshinif22@gmail.com>
2025-11-28 11:07:16 +0200

    add navbar

--- frontend-simplified/src/components/Navbar.jsx
+++ frontend-simplified/src/components/Navbar.jsx
@@ -18,6 +18,7 @@ const Navbar = () => {
                 React Jobs
               </span>
             </NavLink>
+
             <div className='md:ml-auto'>
               <div className='flex space-x-2'>
                 <NavLink to='/' className={linkClass}>
@@ -29,6 +30,14 @@ const Navbar = () => {
                 <NavLink to='/add-job' className={linkClass}>
                   Add Job
                 </NavLink>
+
+                {/* 🔹 NEW AUTH LINKS */}
+                <NavLink to='/login' className={linkClass}>
+                  Login
+                </NavLink>
+                <NavLink to='/signup' className={linkClass}>
+                  Sign Up
+                </NavLink>
               </div>
             </div>
           </div>
@@ -37,4 +46,5 @@ const Navbar = () => {
     </nav>
   );
 };
+
 export default Navbar;
